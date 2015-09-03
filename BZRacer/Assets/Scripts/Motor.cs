9635ad27e8e626199ad697d5ccd6ed44ac22f9dd
Davidott4 <davidott4@gmail.com>
2015-09-02 17:36:16 -0700

    Fixed Wheels

---
+++ BZRacer/Assets/Scripts/Motor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+	/* FL
+	 * FR
+	 * BL
+	 * BR
+	 */
+	public Wheel[] wheels;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		float torque = Input.GetAxis("Vertical") * 20f;
+	}
+}
---
+++ BZRacer/Assets/Scripts/Wheel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(WheelCollider))]
+
+public class Wheel : MonoBehaviour {
+
+
+	WheelCollider wheelCollider;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// FixedUpdate For Pyhsics
+
+}
